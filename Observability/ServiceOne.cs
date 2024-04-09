cc45afb47d46f34f6de7865af20ec7469e56d089
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-09 11:14:30 +0300

    Events [ServiceOne]

--- Observability/ServiceOne.cs
+++ Observability/ServiceOne.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,20 @@ namespace Observability
         static HttpClient httpClient = new HttpClient();
         internal async Task<int> MakeRequestToGoogle()
         {
-            using var activity = ActivitySourceProvider.Source.StartActivity(kind : System.Diagnostics.ActivityKind.Producer, name: "CustomMakeRequestToGoogle");
+            using var activity = ActivitySourceProvider.Source.StartActivity(kind: System.Diagnostics.ActivityKind.Producer, name: "CustomMakeRequestToGoogle");
+
+            var eventTags = new ActivityTagsCollection();
+
+            activity?.AddEvent(new("google'a istek başladı", tags: eventTags));
+
 
             var result = await httpClient.GetAsync("https://www.google.com");
-
             var responseContent = await result.Content.ReadAsStringAsync();
+
+
+            eventTags.Add("google body lenght", responseContent.Length);
+            activity?.AddEvent(new("google'a istek tamamlandı", tags: eventTags));
+
 
             return responseContent.Length;
         }
